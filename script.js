f6c4f00999f728b86efea1302ea855e692ed7aaf
vinaykumarpandey <vinaypandey.mit12@gmail.com>
2024-10-17 16:43:19 +0530

    Fetch Prject

---
+++ script.js
@@ -0,0 +1,27 @@
+function displayCarBrands(response) {
+   const carBrandList = document.getElementById('car-brand-list')
+   carBrandList.innerHTML ='';
+
+   response.forEach(car => {
+     const carBrandDiv = document.createElement('div');
+     carBrandDiv.className = 'car-brand';
+     carBrandDiv.textContent = `Code: ${car.codigo}, Name: ${car.nome}`;
+     carBrandList.appendChild(carBrandDiv);
+   });
+
+}
+
+
+
+
+
+let p = fetch("https://parallelum.com.br/fipe/api/v1/carros/marcas");
+
+p.then((response) => {
+    return response.json();
+}).then((response) => {
+    console.log(response);
+    displayCarBrands(response);
+}).catch(error => {
+    console.log('There was problem fetching the car data ', error)
+})
